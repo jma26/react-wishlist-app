c39ddc13e9e5aab101802731b27a67ae532db22d
Jesse Ma <j.masonoma17@gmail.com>
2018-07-22 22:51:13 -0700

    Add header, form elements to App component

--- wish-list-app/src/App.js
+++ wish-list-app/src/App.js
@@ -6,13 +6,20 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1> wish<span>Li</span>st </h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div class="main">
+          <form>
+            <label for="wish" /> Wish item
+            <input type="text" id="wish" />
+            <label for="link" /> Link
+            <input type="text" id="link" />
+            <label type="image-upload" /> Image
+            <input type="file" id="image-upload" />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     );
   }
