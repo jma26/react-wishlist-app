4e6a037a6ae22c70fa37d4a2fd9377b84ef8e50e
Jesse Ma <j.masonoma17@gmail.com>
2018-07-25 23:24:36 -0700

    Update App component with fieldset elements

--- wish-list-app/src/App.js
+++ wish-list-app/src/App.js
@@ -32,12 +32,18 @@ class App extends Component {
         </header>
         <div className="main">
           <form onSubmit={this.handleSubmit} className="wishform">
-            <label htmlFor="wish" /> Wish item
-            <input type="text" id="wish" onChange={(event) => this.handleChange("item", event)} />
-            <label htmlFor="link" /> Link
-            <input type="text" id="link" onChange={(event) => this.handleChange("link", event)} />
-            <label type="image-upload" /> Image
-            <input type="file" id="image-upload" onChange={(event) => this.handleChange("image", event)}/>
+            <fieldset className="wish-field">
+              <label htmlFor="wish" /> Wish item
+              <input type="text" id="wish" onChange={(event) => this.handleChange("item", event)} />
+            </fieldset>
+            <fieldset className="link-field">
+              <label htmlFor="link" /> Link
+              <input type="text" id="link" onChange={(event) => this.handleChange("link", event)} />
+            </fieldset>
+            <fieldset className="image-field">
+              <label type="image-upload" /> Image
+              <input type="file" id="image-upload" onChange={(event) => this.handleChange("image", event)}/>
+            </fieldset>
             <input type="submit" value="Submit" />
           </form>
           <div className="list">
