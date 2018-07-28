160304365a3b481d381ebc282e0cffaa433813e6
Jesse Ma <j.masonoma17@gmail.com>
2018-07-27 23:31:45 -0700

    Update app.js for multer diskStorage configuration and http post

--- wish-list-server/app.js
+++ wish-list-server/app.js
@@ -1,41 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const uuidv4 = require('uuid/v4');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // Store files in 'uploads' directory
+    cb(null, './uploads');
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Multer instance to upload/save files
+const upload = multer({ storage });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.post('/upload', upload.single('selectedFile'), (req, res) => {
+  console.log(req);
+})
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
