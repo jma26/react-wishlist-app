1742e1e3f9f942b1cca2786888c3d6e512041a47
Jesse Ma <j.masonoma17@gmail.com>
2018-07-27 23:54:34 -0700

    Update filename option for multer diskStorage configuration

--- wish-list-server/app.js
+++ wish-list-server/app.js
@@ -9,20 +9,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const storage = multer.diskStorage({
+  // Store files in 'uploads' directory
   destination: (req, file, cb) => {
-    // Store files in 'uploads' directory
     cb(null, './uploads');
+  },
+  // 
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
   }
 });
 
 // Multer instance to upload/save files
 const upload = multer({ storage });
 
-app.post('/upload', upload.single('selectedFile'), (req, res) => {
+app.post('/upload', upload.single('image'), (req, res) => {
   console.log(req);
 })
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
